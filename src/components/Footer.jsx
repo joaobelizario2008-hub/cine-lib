f0f7f7706743923ed5ed74a5f1541b36acc1292c
João Paulo <joaobelizario2008@gmail.com>
2025-12-11 11:16:24 -0300

    correção do componente footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,12 @@ import "./Footer.css";
 
 const Footer = () => {
     return (
-        <Footer>
+        <footer>
             <p>
             Site desenvolvido na disciplina de desenvolvimento Web I do curso técnico subsequente em informática para internet do ifce - Campos Tauá
             </p>
             <p>CineLib @ 2025 - Exclusivamente para fins educacionais</p>
-        </Footer>
+        </footer>
     );
 };
 
